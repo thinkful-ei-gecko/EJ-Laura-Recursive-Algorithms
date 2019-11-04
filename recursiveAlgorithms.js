42b12b6544dea216f6d3017d3782ffaa18afca4d
EJ <ejg132@gmail.com>
2019-11-04 09:54:24 -0800

    reverse string

--- recursiveAlgorithms.js
+++ recursiveAlgorithms.js
@@ -17,17 +17,38 @@ function allSheep(sheep){
 allSheep(3)
 */
 
-function powerCalculator(base, ex, total=base) {
-    //error case
-    if (ex < 0) {
-        return console.log('exponent should be >= 0')
-    }
-    if (ex === 1){
-    //base case
-    return console.log(total);
-    }
-    total = base * total;
-    powerCalculator(base, ex-1, total);
-}
+//Power Calculator
 
-powerCalculator(2, 5);
+// function powerCalculator(base, ex, total=base) {
+//     //error case
+//     if (ex < 0) {
+//         return console.log('exponent should be >= 0')
+//     }
+//     if (ex === 1){
+//     //base case
+//     return console.log(total);
+//     }
+//     total = base * total;
+//     powerCalculator(base, ex-1, total);
+// }
+
+// powerCalculator(2, 5);
+
+// Reverse String
+
+// function reverseString(string){
+
+//     //base case
+//     if(string === ''){
+//         return console.log('');
+//     }
+
+//     //general case
+//     const char = string[string.length - 1]
+//     console.log(char)
+
+//     return reverseString(string.substring(0, string.length - 1))
+
+// }
+
+// reverseString('dog')
