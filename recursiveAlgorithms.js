83ca2f4aad3ecb58c987df56db31dd404f1862ac
EJ <ejg132@gmail.com>
2019-11-05 08:12:07 -0800

    all problems

--- recursiveAlgorithms.js
+++ recursiveAlgorithms.js
@@ -1,54 +1,319 @@
-// Counting Sheep
-/*
-function allSheep(sheep){
+//1st problem
 
-    //base case
-    if(sheep === 0){
-        return console.log('All sheep jumped over the fence')
+const sheepCounter = (sheepCount) => {
+    if (sheepCount === 0) {
+      console.log('All sheep jumped over the fence');
+      return;
     }
-
-
-    //general case
-    console.log(sheep + ': Another sheep jumps over the fence')
-    allSheep(sheep - 1)
-
-}
-
-allSheep(3)
-*/
-
-//Power Calculator
-
-// function powerCalculator(base, ex, total=base) {
-//     //error case
-//     if (ex < 0) {
-//         return console.log('exponent should be >= 0')
-//     }
-//     if (ex === 1){
-//     //base case
-//     return console.log(total);
-//     }
-//     total = base * total;
-//     powerCalculator(base, ex-1, total);
-// }
-
-// powerCalculator(2, 5);
-
-// Reverse String
-
-// function reverseString(string){
-
-//     //base case
-//     if(string === ''){
-//         return console.log('');
-//     }
-
-//     //general case
-//     const char = string[string.length - 1]
-//     console.log(char)
-
-//     return reverseString(string.substring(0, string.length - 1))
-
-// }
-
-// reverseString('dog')
+  
+    console.log(`${sheepCount}: Another sheep jumps over the fence`);
+    return sheepCounter(sheepCount - 1);
+  };
+  
+  //sheepCounter(3);
+  
+  //2nd problem
+  
+  const powerCalculator = (i, exponent) => {
+    if (exponent === 0) {
+      return 1;
+    }
+    if (exponent < 0) {
+      return 'exponent should be >= 0';
+    }
+  
+    return i * powerCalculator(i, exponent - 1);
+  };
+  
+  //const powerAnswer = powerCalculator(10, 2);
+  //console.log(powerAnswer);
+  //const noAnswer = powerCalculator(10, -2);
+  //console.log(noAnswer);
+  
+  //3rd problem
+  
+  const reverseString = (string) => {
+    if (!string.length) {
+      return '';
+    }
+  
+    return string[string.length - 1] + reverseString(string.slice(0, string.length - 1));
+  };
+  
+  //const reverseStringAnswer = reverseString('elephant');
+  //console.log(reverseStringAnswer);
+  
+  //4th problem
+  function triangularNumber(n) {
+    if (n === 1)
+      return n;
+  
+    return n + triangularNumber(n - 1);
+  }
+  
+  /* console.log(`triangularNumber: ${triangularNumber(3)}`);
+  console.log(`triangularNumber: ${triangularNumber(4)}`);
+  console.log(`triangularNumber: ${triangularNumber(5)}`);
+  console.log(`triangularNumber: ${triangularNumber(6)}`);
+  console.log(`triangularNumber: ${triangularNumber(7)}`);
+  console.log(`triangularNumber: ${triangularNumber(8)}`); */
+  
+  // 5. string splitter
+  function stringSplitter(string, separator) {
+    let index = string.indexOf(separator);
+  
+    if (index === -1)
+      return [string];
+  
+    return [string.slice(0, index)].concat(
+      stringSplitter(
+        string.slice(index + separator.length), separator));
+  }
+  
+  //console.log(stringSplitter('20/20/2020', 's'));
+  
+  // 6. fibonacci
+  function fibonacci(num) {
+    if (num === 0)
+      return 'pick a number greater than or equal to 1'
+  
+    if (num <= 2)
+      return 1;
+  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+  
+  // 7. factorial
+  function factorial(num) {
+    if (num === 0)
+      return 1;
+    if (num === 1)
+      return 1;
+    return num * factorial(num - 1);
+  }
+  
+  
+  // 8. maze
+  
+  let mySmallMaze = [
+    [' ', ' ', ' '],
+    [' ', '*', ' '],
+    [' ', ' ', 'e']
+  ];
+  
+  let myLargeMaze = [
+    [' ', ' ', ' ', '*', ' ', ' ', ' '],
+    ['*', '*', ' ', '*', ' ', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+  ];
+  
+  const maze = (x = 0, y = 0, mazeArr) => {
+    const rows = mazeArr.length - 1;
+    const columns = mazeArr[0].length - 1;
+  
+    if (x < 0 || x > columns || y < 0 || y > rows)
+      return 'Please use a x and y value >= 0';
+  
+    if (mazeArr[y][x] === 'e')
+      return ` Yay! Reached the goal at ${x}:${y}`;
+  
+    if (mazeArr[y][x + 1] !== '*' && x < columns) {
+      mazeArr[y][x] = '*';
+      return 'R' + maze(x + 1, y, mazeArr);
+    }
+  
+    if (mazeArr[y][x - 1] !== '*' && x >= 0) {
+      mazeArr[y][x] = '*';
+      return 'L' + maze(x - 1, y, mazeArr);
+    }
+  
+    if (mazeArr[y + 1][x] !== '*' && y < rows) {
+      mazeArr[y][x] = '*';
+      return 'D' + maze(x, y + 1, mazeArr);
+    }
+  
+    if (mazeArr[y - 1][x] !== '*' && y >= 0) {
+      mazeArr[y][x] = '*';
+      return 'U' + maze(x, y - 1, mazeArr);
+    }
+  };
+  
+  //console.log(maze(0, 0, mySmallMaze));
+  //console.log(maze(0, 0, myLargeMaze));
+  
+  // 9. Maze continued...
+  
+  /* const allExitsMaze = (x, y, solves = 0, mazeArr) => {
+    const rows = mazeArr.length -1;
+    const columns = mazeArr[0].length -1;
+    if (x < 0 || x > columns || y < 0 || y > rows)
+      return 'Please use a x and y value >= 0';
+    if (mazeArr[y][x] === 'e')
+      return ` Yay! Reached the goal at ${x}:${y}`;
+    if(mazeArr[y][x + 1] === ' ' && x < columns){
+      mazeArr[y][x] = `${solves}`;
+      return 'R' + allExitsMaze(x+1, y, mazeArr);
+    }
+    if(mazeArr[y + 1][x] === ' ' && y < rows){
+      mazeArr[y][x] = `${solves}`;
+      return 'D' + allExitsMaze(x, y+1, mazeArr);
+    }
+    if(mazeArr[y][x - 1] === ' ' && x >= 0){
+      mazeArr[y][x] = `${solves}`;
+      return 'L' + allExitsMaze(x-1, y, mazeArr);
+    }
+    if(mazeArr[y - 1][x] === ' ' && y >= 0){
+      mazeArr[y][x] = `${solves}`;
+      return 'U' + allExitsMaze(x, y-1, mazeArr);
+    }
+  }; */
+  
+  const allExitsMaze = (mazeArr, position = 0, row, column, direction = 'S', path) => {
+    if (column < 0 || row < 0)
+      return 'Out of bounds!';
+  
+    if (column >= mazeArr[0].length || row >= mazeArr.length)
+      return 'Out of bounds!';
+  
+    path[position] = direction;
+    position++;
+  
+    if (mazeArr[row][column] === 'e') {
+      //console.log(`${path}`);
+      printer(path, 1, position - 1);
+      return;
+    }
+  
+    if (mazeArr[row][column] === ' ') {
+      mazeArr[row][column] = 's';
+      allExitsMaze(mazeArr, position, row, column - 1, 'L', path);
+      allExitsMaze(mazeArr, position, row - 1, column, 'U', path);
+      allExitsMaze(mazeArr, position, row, column + 1, 'R', path);
+      allExitsMaze(mazeArr, position, row + 1, column, 'D', path);
+      mazeArr[row][column] = ' ';
+    }
+    position = position - 1;
+  };
+  
+  const printer = (path, startPos, endPos) => {
+    console.log('Found path: ');
+    console.log(`${path}`);
+  };
+  //allExitsMaze(mySmallMaze);
+  console.log(allExitsMaze(myLargeMaze, 0, 0, 0, '', []));
+  
+  // 10. Anagrams
+  function anagrams(prefix, str) {
+    if (str.length <= 1) {
+      console.log(`The anagram is ${prefix}${str}`);
+    } else {
+      for (let i = 0; i < str.length; i++) {
+        let currentLetter = str.substring(i, i + 1);
+        let previousLetters = str.substring(0, i);
+        let afterLetters = str.substring(i + 1);
+        anagrams(prefix + currentLetter, previousLetters + afterLetters);
+      }
+    }
+  }
+  function printAnagram(word) {
+    anagrams(' ', word);
+  }
+  
+  // printAnagram('east');
+  
+  // 11. organization chart
+  
+  let organization = {
+    'Zuckerberg': {
+      'Schroepfer': {
+        'Bosworth': {
+          'Steve': {},
+          'Kyle': {},
+          'Andra': {}
+        },
+        'Zhao': {
+          'Richie': {},
+          'Sofia': {},
+          'Jen': {}
+        },
+        'Badros': {
+          'John': {},
+          'Mike': {},
+          'Pat': {}
+        },
+        'Parikh': {
+          'Zach': {},
+          'Ryan': {},
+          'Tes': {}
+        }
+      },
+      'Schrage': {
+        'VanDyck': {
+          'Sabrina': {},
+          'Michelle': {},
+          'Josh': {}
+        },
+        'Swain': {
+          'Blanch': {},
+          'Tom': {},
+          'Joe': {}
+        },
+        'Frankovsky': {
+          'Jasee': {},
+          'Brian': {},
+          'Margaret': {}
+        }
+      },
+      'Sandberg': {
+        'Goler': {
+          'Eddie': {},
+          'Julie': {},
+          'Annie': {}
+        },
+        'Hernandez': {
+          'Rowi': {},
+          'Inga': {},
+          'Morgan': {}
+        },
+        'Moissinac': {
+          'Amy': {},
+          'Chuck': {},
+          'Vinni': {}
+        },
+        'Kelley': {
+          'Eric': {},
+          'Ana': {},
+          'Wes': {}
+        }
+      }
+    }
+  };
+  function traverseA(data, depth = 0) {
+    let indent = ' '.repeat(depth * 4);
+    Object.keys(data).forEach(key => {
+      console.log(indent + key);
+      traverseA(data[key], depth + 1);
+    });
+  }
+  
+  function traverseB(node, indent = 0) {
+    for (let key in node) {
+      console.log(' '.repeat(indent), key);
+      traverseB(node[key], indent + 4);
+    }
+  }
+  
+  // traverseA(organization);
+  // traverseB(organization);
+  
+  function binaryRep(input) {
+    if(input <= 0) {
+      return '';
+    }
+    let binary = Math.floor(input%2);
+    return binaryRep(Math.floor(input/2)) + binary;
+  }
+  
+  // console.log(binaryRep(25));
